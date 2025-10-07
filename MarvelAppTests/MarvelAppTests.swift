3d4beab86a7c09a5d10a81f2bcef16879b2eadcc
Ivan Tonial <ivan.tonial@ip.tv>
2025-10-07 09:15:16 -0300

    Initial Commit

---
+++ MarvelAppTests/MarvelAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppTests.swift
+//  MarvelAppTests
+//
+//  Created by Ivan Tonial IP.TV on 07/10/25.
+//
+
+import Testing
+@testable import MarvelApp
+
+struct MarvelAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
