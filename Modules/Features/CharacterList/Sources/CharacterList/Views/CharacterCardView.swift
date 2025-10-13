64c89180eddc2c131999b7f6e33d6aadbf4ba7d6
Ivan Tonial <ivan.tonial@ip.tv>
2025-10-13 09:04:45 -0300

    Deleting commented code

--- Modules/Features/CharacterList/Sources/CharacterList/Views/CharacterCardView.swift
+++ Modules/Features/CharacterList/Sources/CharacterList/Views/CharacterCardView.swift
@@ -5,42 +5,6 @@
 //  Created by Ivan Tonial IP.TV on 07/10/25.
 //
 
-//import DesignSystem
-//import MarvelAPI
-//import SwiftUI
-//
-//public struct CharacterCardView: View {
-//    let model: CharacterCardModel
-//
-//    public init(model: CharacterCardModel) {
-//        self.model = model
-//    }
-//
-//    public var body: some View {
-//        ContentCardComponent(
-//            model: model.toContentCardModel(),
-//            onTap: nil // O tap será tratado pelo CharacterListView
-//        )
-//    }
-//}
-//
-//// MARK: - Model Extension
-//private extension CharacterCardModel {
-//    func toContentCardModel() -> ContentCardModel {
-//        ContentCardModel(
-//            id: id,
-//            title: name,
-//            subtitle: nil,
-//            imageURL: imageURL,
-//            aspectRatio: 1.0, // Quadrado para personagens
-//            badge: ContentCardModel.BadgeModel(
-//                icon: "book.fill",
-//                text: "\(comicsCount) comics",
-//                color: .red
-//            )
-//        )
-//    }
-//}
 import DesignSystem
 import MarvelAPI
 import SwiftUI
